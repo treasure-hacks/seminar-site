439be6762d1c420bb900f166950b0b068addea79
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2024-01-14 12:48:06 -0800

    update registration links based on ref

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,1 +1,9 @@
+'use client'
 
+window.addEventListener('load',  () => {
+  const ref = new URL(location.href).searchParams.get('ref')
+  if (!ref) return
+
+  document.querySelectorAll<HTMLAnchorElement>('a[href$="/register/"]')
+    .forEach(a => { a.href += '?ref=' + encodeURIComponent(ref) })
+})
