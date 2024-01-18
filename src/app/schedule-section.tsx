c3fd8923aa410fe26c7566c5f5e2296b445e1040
Ethan Xie <79242981+Anonymouseyy@users.noreply.github.com>
2024-01-17 17:15:23 -0800

    formatting for mobile

--- src/app/schedule-section.tsx
+++ src/app/schedule-section.tsx
@@ -62,7 +62,7 @@ const schedule: EventItemProps[] = [
 export function ScheduleSection(){
 
     return (
-        <div id="schedule" className="xl:px-[180px] lg:px-[50px] md:px-[20px] relative flex flex-col items-center lg:items-center px-14 max-[400px]:px-3 py-10">
+        <div id="schedule" className="xl:px-[180px] lg:px-[50px] md:px-[20px] relative flex flex-col items-center lg:items-center px-14 max-[450px]:px-3 py-10">
             <h2 className='text-4xl mt-8 mb-8 text-center'>Schedule</h2>
             {schedule.map((event: EventItemProps) => <Leaf key={ event.title } title={ event.title } time={ event.time } duration={ event.duration } description={ event.description }/>)}
         </div>
@@ -73,8 +73,8 @@ function Leaf({ title, time, duration, description }: EventItemProps){
     return (
         <>
             <li  key={title} className="flex w-full text-white flex-row-reverse">
-                <div className={"w-[50%] flex border-white-16 flex-row border-l-8 px-[2%] py-5"}>
-                    <div className={"flex flex-col font-normal w-[80%] text-left content-center"}>
+                <div className={"w-[50%] max-[450px]:w-[56%] flex border-white-16 flex-row border-l-8 px-[2%] py-5"}>
+                    <div className={"flex flex-col font-normal w-[100%] text-left content-center"}>
                         <div className="flex flex-row">
                             <Image src="clock.svg" width="24" height="24" alt="clock"></Image>
                             <p className="text-l">{duration}</p>
@@ -84,8 +84,8 @@ function Leaf({ title, time, duration, description }: EventItemProps){
                     </div>
                 </div>
 
-                <div className="flex flex-row items-center flex-auto justify-end">
-                    <p className="text-3xl font-semibold mr-[3%] max-[450px]:text-2xl">{time}</p>
+                <div className="flex flex-row items-center flex-auto justify-end w-[50%] max-[450px]:w-[44%]">
+                    <p className="text-3xl font-semibold mr-[3%] max-[457px]:text-2xl">{time}</p>
                     <div className="w-[5%] aspect-square rounded-full bg-white relative left-2"></div>
                     <div className="w-[15%] border border-solid border-t-8"></div>
                 </div>
