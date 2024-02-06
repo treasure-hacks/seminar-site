8cd397919a47a7b52aa3d47e0a301a7f19f7daed
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2024-02-05 17:00:05 -0800

    add other sponsors (#17)
    hopefully they still look good enough

--- src/app/sponsors-section.tsx
+++ src/app/sponsors-section.tsx
@@ -22,6 +22,29 @@ export function SponsorsSection() {
           <Image src='/images/sponsors/hyperx.png' width='200' height='50' alt='HyperX Logo'/>
         </div>
       </div>
+      <div className="flex flex-wrap gap-12 items-center justify-evenly w-full max-[927px]:max-w-[410px] max-[927px]:gap-[2.5rem_2rem] mt-3">
+        <div className="flex flex-col gap-6">
+          <Image src='/images/sponsors/ucsd-basement.png' width='260' height='72' alt='UCSD Basement Logo' className="max-[927px]:w-[220px]"/>
+          <div className="flex justify-between">
+            <Image src='/images/sponsors/ucsd-ostem.png' width='170' height='72' alt='UCSD oSTEM Logo' className="max-[927px]:w-[150px]"/>
+            <Image src='/images/sponsors/ucsd-pib.png' width='72' height='72' alt='Project in a Box Logo' className="max-[927px]:w-[60px]"/>
+          </div>
+        </div>
+        <div className="flex flex-col gap-6">
+          <Image src='/images/sponsors/ucsd-cse-society.png' width='172' height='80' alt='UCSD CSE Society Logo'
+            className="max-[927px]:w-[144px]"/>
+          <Image src='/images/sponsors/ucsd-ewh.png' width='172' height='80' alt='UCSD Engineering World Health Logo'
+            className="max-[927px]:w-[144px]"/>
+        </div>
+        <div className="flex flex-col gap-6">
+          <Image src='/images/sponsors/ucsd-blackstone.png' width='240' height='40' alt='Blackstone Launchpad Logo'
+            className="max-[927px]:w-[350px] max-[475px]:w-[260px]"/>
+          <Image src='/images/sponsors/marchitecture.png' width='240' height='60' alt='Marchitecture Logo'
+            className="max-[927px]:w-[350px] max-[475px]:w-[260px]"/>
+          <Image src='/images/sponsors/mohuman.png' width='240' height='60' alt='Mohuman Logo'
+            className="max-[927px]:w-[350px] max-[475px]:w-[260px]"/>
+        </div>
+      </div>
     </div>
   </div>
 }
