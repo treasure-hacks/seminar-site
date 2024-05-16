def53f41d03890aabacea1f3f8f6876736c2ad3d
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2024-05-15 18:25:34 -0700

    Add testimonial section

--- src/app/winter2024/page.tsx
+++ src/app/winter2024/page.tsx
@@ -3,6 +3,7 @@ import Script from 'next/script'
 import PageHeader from './PageHeader';
 import StatsSection from './StatsSection';
 import EventHighlightsSection from './EventHighlightsSection';
+import TestimonialsSection from './TestimonialsSection';
 // import './index'
 
 export default function Home() {
@@ -10,11 +11,11 @@ export default function Home() {
     <>
       <main className='bg-gradient-to-b from-[#3D66AB] to-[#1E2630]'>
         <div className='overflow-x-clip'>
-          <PageHeader/>
+          <PageHeader />
         </div>
-        <StatsSection/>
+        <StatsSection />
         <EventHighlightsSection />
-        
+        <TestimonialsSection />
       
         {/*
 
